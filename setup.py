23d42b99dc0b7effc60d9600d8bba3a6980ac6bb
wuemily <emilyywu@umich.edu>
2023-10-08 17:01:46 -0400

    setup.py changes

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = imu_logger.publisher_member_function:main',
+            'imu_logging = imu_logger.imu_logging:main',
         ],
     },
 )
